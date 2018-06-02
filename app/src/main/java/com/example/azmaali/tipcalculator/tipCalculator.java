f9151ce3f28dd9272909996b131449e9e483ee35
Azma Ali <azmaali0@gmail.com>
2018-06-02 19:42:43 -0400

    revamped the app by adding the option to split the bill and round totals

--- app/src/main/java/com/example/azmaali/tipcalculator/tipCalculator.java
+++ app/src/main/java/com/example/azmaali/tipcalculator/tipCalculator.java
@@ -2,68 +2,96 @@ package com.example.azmaali.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
 import java.text.NumberFormat;
+import android.widget.RadioGroup;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.AdapterView;
+
+
 
 public class tipCalculator extends AppCompatActivity {
 
-    private float tipPercent = 0.15f;
-    private float totalAmount = 0;
-    private float tipAmount = 0;
     private EditText billAmountEditText;
     private TextView tipPercentTextView;
     private TextView totalAmountTextView;
     private TextView tipAmountTextView;
-    private Button percentUpButton;
-    private Button percentDownButton;
+    private TextView perPersonAmountTextView;
+    private SeekBar seekBar;
+    private RadioGroup roundingRadioGroup;
+    private Spinner splitBillSpinner;
+
+
+    private float tipPercent = 0.15f;
+    private float totalAmount = 0;
+    private float tipAmount = 0;
+    private int rounding;
+    private float perPersonAmount;
+    private int split;
+
+    private final int ROUND_NONE = 0;
+    private final int ROUND_TIP = 1;
+    private final int ROUND_TOTAL = 2;
+    private final int NO_SPLIT = 0;
+    private final int TWO_WAY_SPLIT = 1;
+    private final int THREE_WAY_SPLIT = 2;
+    private final int FOUR_WAY_SPLIT = 3;
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_tip_calculator);
         billAmountEditText = findViewById(R.id.billAmountEditView);
         tipPercentTextView = findViewById(R.id.tipPercentTextView);
         totalAmountTextView =  findViewById(R.id.totalAmountTextView);
         tipAmountTextView = findViewById(R.id.tipAmountTextView);
+        roundingRadioGroup = findViewById(R.id.roundingRadioGroup);
+        splitBillSpinner = findViewById(R.id.splitBillSpinner);
+        perPersonAmountTextView = findViewById(R.id.perPersonTextView);
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(85);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.splitbill_array,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        splitBillSpinner.setAdapter(adapter);
 
-        percentUpButton = (Button) findViewById(R.id.percentUpButton);
-        percentDownButton = (Button) findViewById(R.id.percentDownButton);
 
 
 
-
-        percentDownButton.setOnClickListener(new View.OnClickListener() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 15;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progress += progressChanged;
+                tipPercentTextView.setText(String.valueOf(progress+"%"));
+            }
 
             @Override
-            public void onClick(View v) {
-                if (tipPercent > .14) {
-                    tipPercent = tipPercent - 0.01f;
-                    calculateTotals();
-                }
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //TODO Auto-generated method stub
             }
-        });
-
-
-        percentUpButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v) {
-                if (tipPercent < 1) {
-                    tipPercent = tipPercent + 0.01f;
-                    calculateTotals();
-                }
-            }
-        });
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int progress = seekBar.getProgress();
+                tipPercent = (float) (progress+progressChanged)/100;
+                calculateTotals();
 
+            }
+
+        });
 
         billAmountEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -72,6 +100,40 @@ public class tipCalculator extends AppCompatActivity {
                 return false;
             }
         });
+
+        roundingRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.noRoundingRadioButton){
+                    calculateTotals();
+                }
+                else if (checkedId == R.id.tipRoundingRadioButton){
+                    rounding = ROUND_TIP;
+                    calculateTotals();
+                }
+
+                else if (checkedId == R.id.totalRoundingRadioButton){
+                    rounding = ROUND_TOTAL;
+                    calculateTotals();
+
+                }
+            }
+
+        });
+
+        splitBillSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                split = position + 1;
+                calculateTotals();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
 
     }
@@ -86,15 +148,37 @@ public class tipCalculator extends AppCompatActivity {
            billAmount = Float.parseFloat(billAmountStr);
         }
 
+        //Calculate totals
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
         tipAmount = tipPercent * billAmount;
         totalAmount = billAmount+ tipAmount;
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        tipAmountTextView.setText(currency.format(tipAmount));
-        totalAmountTextView.setText(currency.format(totalAmount));
-        NumberFormat percent = NumberFormat.getPercentInstance();
-        tipPercentTextView.setText(percent.format(tipPercent));
+        perPersonAmount = totalAmount/split;
+
+        //Check rounding
+        if (rounding == ROUND_TOTAL){
+            totalAmount = Math.round(totalAmount);
+
+        }
+        else if (rounding == ROUND_TIP){
+            tipAmount = Math.round(tipAmount);
+        }
+
+
+        // Display amounts
+        if (split > 1 && !billAmountStr.isEmpty()){
+            perPersonAmountTextView.setText(String.format("%.2f",perPersonAmount));
+        }
+        else {
+            perPersonAmountTextView.setText("");
+        }
+        tipAmountTextView.setText(String.format("%.2f", tipAmount));
+        totalAmountTextView.setText(String.format("%.2f", totalAmount));
+
+
 
     }
+
+
 
 }
 
